dc39badd61e242c8baf56fdd9379f0a374c745ab
diamondwarranty <diamondwarrantyc@gmail.com>
2025-10-05 01:10:56 +0600

    Update sanity.cli.js

--- diamondwarranty/sanity.cli.js
+++ diamondwarranty/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '96qtlbls',
+    projectId: 'q26e4dqa',
     dataset: 'production'
   },
   /**
