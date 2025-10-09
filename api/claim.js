4685743696c9a65bb7771a3a4caf722b83d31c32
diamondwarranty <diamondwarrantyc@gmail.com>
2025-10-09 22:14:28 +0600

    Update claim.js
    change email back to actual from test

--- api/claim.js
+++ api/claim.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
 
     await resend.emails.send({
       from: "Claims Form <onboarding@diamondwarrantycorp.com>", // must be verified
-      to: "support@ahsantaz.com",
+      to: "claims@diamondwarrantycorp.com",
       subject: "📩 New Claim Form Submission",
       html,
     });
